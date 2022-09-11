e415ad483a7925df72e6a67df06b718f10e7198a
Juan Alvarado <jujalvarado@est.istg.edu.ec>
2022-09-11 18:05:41 -0500

    2do avance

--- bazar/producto/admin.py
+++ bazar/producto/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Categoria_producto,Proveedor, Marca,Proveedor_marca, Producto, Producto_imagen
+
+admin.site.register(Categoria_producto)
+admin.site.register(Proveedor)
+admin.site.register(Marca)
+admin.site.register(Proveedor_marca)
+admin.site.register(Producto_imagen)
+
+
--- bazar/producto/models.py
+++ bazar/producto/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Categoria_producto(models.Model):
+    codigo= models.CharField(primary_key=True, max_length=6)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.codigo)
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=250)
+    estado = models.SmallIntegerField(max_length=1, default=1)   
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.descripcion)
+
+
+class Proveedor(models.Model):
+    nombre =  models.CharField(max_length=50)
+    descripcion= models.TextField(max_length= 250)
+    ruc = models.CharField(max_length=13)
+    direccion = models.TextField(max_length= 250)
+    celular = models.CharField(max_length=10)
+    email = models.EmailField()
+    estado = models.SmallIntegerField(max_length=1, default=1)   
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.descripcion)
+
+
+class Proveedor_marca(models.Model):
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    estado = models.SmallIntegerField(max_length=1, default=1)  
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.proveedor, self.marca)
+    
+         
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria_producto,on_delete=models.CASCADE)
+    descripcion = models.TextField(max_length= 250)
+    proveedor = models.ForeignKey(Proveedor_marca, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    precio_compra = models.DecimalField(max_digits=4,decimal_places=2)
+    precio_venta = models.DecimalField(max_digits=4,decimal_places=2)
+    estado = models.SmallIntegerField(max_length=1, default=1)   
+    
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.stock)
+
+
+class Producto_imagen(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='fotos/productos',blank=True)
+    
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(Producto.nombre, Producto.descripcion)
